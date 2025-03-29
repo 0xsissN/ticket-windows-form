f0d0b9929e74d44eb9d6e6fd1418dd101ac7093f
0xsissN <nico22delcastillo07@gmail.com>
2025-03-29 19:39:49 -0400

    index

--- ticket-windows-form/Form1.cs
+++ ticket-windows-form/Form1.cs
@@ -16,5 +16,30 @@ namespace ticket_windows_form
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
